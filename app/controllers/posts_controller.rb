1f4c3a7e685cb4d1928637738be31b137393c9e4
Andrea Miranda <amiranda2510@gmail.com>
2021-03-09 18:43:44 -0500

    added methods update and edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,11 +13,25 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to @post
     else
-      render new
+      render 'new'
     end
   end
 
   def show
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
     @post = Post.find(params[:id])
   end
 
